8b8c87e38438508cc1945b0af5bcaaf82b8802c8
Дмитрий Золотухин <dimonnumber1@gmail.com>
2022-07-01 17:24:46 +0200

    asd

--- cypress/e2e/step.cy.js
+++ cypress/e2e/step.cy.js
@@ -1,4 +1,4 @@
-import page from "../page/page"
+const page = require('../page/page.js')
 
 describe('cypress test', () => {
   it('should login and be in plans', () => {
@@ -8,16 +8,22 @@ describe('cypress test', () => {
   })
 
   it('should login and be in keywords', () => {
+    page.open()
+    page.login()
     page.clickOn('Keywords')
     page.check('/functions')
   })
 
   it('should login and be in parameters', () => {
+    page.open()
+    page.login()
     page.clickOn('Parameters')
     page.check('/parameters')
   })
 
   it('should login and be in Schedules', () => {
+    page.open()
+    page.login()
     page.clickOn('Scheduler')
     page.check('/scheduler')
   })
